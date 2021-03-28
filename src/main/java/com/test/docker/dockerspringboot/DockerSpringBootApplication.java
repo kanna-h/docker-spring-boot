cbf23a1229d1f2cb75e08c7ab68eb7e004cd0d33
KANNA <harikrishna010203@gmail.com>
2021-03-28 07:17:43 -0400

    spring initialization- first commit

---
+++ src/main/java/com/test/docker/dockerspringboot/DockerSpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.test.docker.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootApplication.class, args);
+			System.out.println("Ah...");
+			System.out.println("Ahh...");
+			System.out.println("Ahhhh...");
+			System.out.println("Ahhhhh...");
+			System.out.println("Ahhhhhhh...");
+	}
+
+}
