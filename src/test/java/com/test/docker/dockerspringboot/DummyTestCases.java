e69ecd00cc1752e4ec713299ee7996e2a79d4146
KANNA <harikrishna010203@gmail.com>
2021-03-28 07:53:22 -0400

    Test cases added

---
+++ src/test/java/com/test/docker/dockerspringboot/DummyTestCases.java
@@ -0,0 +1,13 @@
+package com.test.docker.dockerspringboot;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DummyTestCases {
+    @Test
+    public void dTest(){
+        System.out.println("*********DUMMY TESTCASE***********");
+        assertEquals(5, 5);
+
+    }
+}
